d84e0cda53a2ded9ac50d607cb7a53dac5880e34
peter <peter.gromov@jetbrains.com>
2010-01-31 14:01:29 +0000

    shorten lexicon

--- src/constructor/Construction.groovy
+++ src/constructor/Construction.groovy
@@ -6,10 +6,19 @@ package constructor
 class Construction {
   String name
   List<Construction> args
+  private Map<List, Closure> expectations = [:]
+  private Set pings = [] as Set
 
   def Construction(String name, List<Construction> args) {
     this.name = name;
     this.args = args;
+  }
+
+  def Construction(name, args, expectations, pings) {
+    this.name = name;
+    this.args = args;
+    this.expectations = expectations;
+    this.pings = pings;
   }
 
   def String toString() {
@@ -22,12 +31,12 @@ class Construction {
 
   String prettyPrint(Closure varName, String indent) {
     def prettyName = name.contains(' ') ? "'$name'" : name
-    def a = [prettyName] + args.collect({ arg ->
+    def a = [prettyName] + args.collect({arg ->
       def prefix = varName(arg)
       if (!prefix || prefix.endsWith("=")) {
         return prefix + arg.prettyPrint(varName, indent + "  ")
       }
-      return prefix 
+      return prefix
     })
 
     String separator = oneLiner() ? " " : "\n$indent  "
@@ -35,10 +44,32 @@ class Construction {
   }
 
   def ping(message) {
+    pings.contains(message)
   }
 
   def activate(ParsingContext ctx) {
-    
+    expectations.each { pattern, action -> ctx.expect(pattern, action) }
+  }
+
+  Construction aka(Object ... msg) {
+    Set pings = msg as Set
+    pings.addAll(this.pings)
+    return new Construction(name, args, expectations, pings)
+  }
+
+  Construction expect(List pattern, Closure action) {
+    for (i in 0..pattern.size()-1) {
+      if (pattern[i] == "_") pattern[i] = this
+    }
+    Map expectations = this.expectations.clone()
+    expectations[pattern] = action
+    def result = new Construction(name, args, expectations, pings)
+    result.expectations.each { k, v ->
+      for (i in 0..k.size()-1) {
+        if (k[i] == this) k[i] = result
+      }
+    }
+    return result
   }
 
 }
--- src/constructor/russian/RussianLexicon.groovy
+++ src/constructor/russian/RussianLexicon.groovy
@@ -2,7 +2,6 @@ package constructor.russian
 
 import constructor.Construction
 import constructor.Lexicon
-import constructor.Space
 import constructor.Word
 import constructor.ParsingContext
 
@@ -12,42 +11,14 @@ import constructor.ParsingContext
 class RussianLexicon extends Lexicon {
 
   def RussianLexicon() {
-    storage["в течение"] = { return new Word("в течение") {
-
-      def Object activate(ParsingContext ctx) {
-        ctx.expect([this, Noun]) { new Construction("Prepos", it) {
-
-          def Object activate(ParsingContext ct) {
-            ct.expect([this, "clause"]) {
-              new Construction("When", it)
-              }
-          }
-
-        } }
+    storage["в течение"] = { new Word("в течение").expect(["_", "noun"]) {
+      new Construction("Prepos", it).expect(["_", "clause"]) {
+        new Construction("When", it)
       }
-
     } }
-    storage["Власти"] = { return new Noun("Власти") {
-
-      def Object ping(Object message) {
-        if (message == "nominative") {
-          return true
-        }
-        return super.ping(message);
-      }
-
-    } }
-    storage["московской"] = { return new Word("московской") {
-                                                                                        
-      def Object activate(ParsingContext ctx) {
-        ctx.expect([this, Noun]) { new Construction("AdjNoun", it) }
-      }
-
-    } }
-    storage["управы"] = { return new Noun("управы") {
-
-      def Object activate(ParsingContext ctx) {
-        ctx.expect([Noun, this]) { new Construction("NounGenitive", it) {
+    storage["Власти"] = { new Word("Власти").aka("nominative", "noun") }
+    storage["московской"] = { new Word("московской").expect(["_", "noun"]) { new Construction("AdjNoun", it) } }
+    storage["управы"] = { new Word("управы").aka("noun").expect(["noun", "_"]) { new Construction("NounGenitive", it) {
 
           def Object activate(ParsingContext ct) {
             ct.reactivate(args[0])
@@ -55,74 +26,19 @@ class RussianLexicon extends Lexicon {
 
         } }
       }
-
-    } }
-    storage['\"'] = { return new Quote() }
-    storage['Крылатское'] = { return new Word('Крылатское') }
+    storage['"'] = { new Word('"').aka('"').expect(["_", Construction, '"']) { return new Quoted(it) } }
+    storage['Крылатское'] = { new Word('Крылатское') }
     storage['намерены'] = { 
-      return new Word('намерены') {
-
-      def Object activate(ParsingContext ctx) {
-        ctx.expect(["nominative", this]) { new Construction("SubjPred", it) }
-        ctx.expect([this, "infinitive"]) { new Construction("XComp", it) }
+      new Word('намерены').expect(["nominative", "_"]) { new Construction("SubjPred", it) }.expect(["_", "infinitive"]) { new Construction("XComp", it) }
       }
 
-    } }
-
-    storage['месяца'] = { new Noun("месяца") }
-    storage['решить'] = { new Verb("решить") {
-
-      def Object ping(Object message) {
-        if (message == "infinitive" || message == "clause") {
-          return true
-        }
-        return super.ping(message);
-      }
-
-    } }
+    storage['месяца'] = { new Word("месяца").aka("noun") }
+    storage['решить'] = { new Word("решить").aka("infinitive", "clause") }
   }
 
   Construction recognize(String s) {
-    if ((s.endsWith("ое")
-            || s.endsWith("ой")
-            || s.endsWith("ых")
-            || s.endsWith("ть")
-            || s.endsWith("ий")
-    ) && s.size() > 3) {
-      return new Inflection("-${s[-2..-1]}", stem(s[0..-3]))
-    }
-    if ((s.endsWith("и") ||
-            s.endsWith("ы") ||
-            s.endsWith("а") ||
-            s.endsWith("е")) && s.size() > 2) {
-      return new Inflection("-${s[-1..-1]}", stem(s[0..-2]))
-    }
-
     return null
   }
-
-  def stem(s) {
-    if ("месяц" == s
-            || "вопрос" == s
-            || "снос" == s
-    ) {
-      return new Word(s)
-    }
-    return new Word("$s-")
-  }
-
-}
-
-class Quote extends Construction {
-
-  def Quote() {
-    super('\"', []);
-  }
-
-  def Object activate(ParsingContext ctx) {
-    ctx.expect([this, Construction, Quote], { return new Quoted(it) })
-  }
-
 
 }
 
@@ -132,24 +48,12 @@ class Quoted extends Construction {
     super("Quoted", args);
   }
 
-  def Object ping(Object message) {
-    return super.ping(message);
-  }
-
   def Object activate(ParsingContext ctx) {
     ctx.deactivate(args[0])
     ctx.deactivate(args[1])
     ctx.deactivate(args[2])
-    ctx.expect([Noun, this]) { return new Construction("Appos", it) }
+    ctx.expect(["noun", this]) { return new Construction("Appos", it) }
   }
 
-
-}
-
-class Verb extends Construction {
-
-  def Verb(name) {
-    super(name, [])
-  }
 
 }

eec2732472012c7a0add209e630eb7ef2e07207e
peter <peter.gromov@jetbrains.com>
2010-03-07 14:11:30 +0000

    extract all the expect/evokes functionality into separate QueryBlock

--- src/constructor/Descriptor.groovy
+++ src/constructor/Descriptor.groovy
@@ -1,7 +1,5 @@
 package constructor
 
-import constructor.util.Pair
-
 /**
  * @author peter
  */
@@ -10,8 +8,7 @@ class Descriptor {
   private def famous = false
   private def tracked = false
   private Set<String> pings = [] as Set
-  protected Map<List, Descriptor> expectations = [:]
-  protected Set<List> optional = [] as Set //too make private when Groovy fixes the bug
+  protected QueryBlock queries = new QueryBlock(true) //too make private when Groovy fixes the bug
   Set<Integer> consumedArgs = [] as Set
   Set<Integer> demotedArgs = [] as Set
   private int _replaces = -1
@@ -39,29 +36,12 @@ class Descriptor {
   }
 
   Descriptor expect(List pattern, action, boolean optional = false) {
-    expectations[pattern] = action
-    if (optional) {
-      this.optional << pattern
-    }
+    queries.expect(pattern, action, optional)
     return this
   }
 
   Descriptor expect(Map<?, Integer> pattern, Descriptor action, boolean optional = false) {
-    def list = pattern.keySet() as List
-    expectations[list] = new Descriptor(action.name) {
-
-      def Construction build(List<Construction> args) {
-        def permuted = new Construction[args.size()] as List
-        args.eachWithIndex { arg, i ->
-          permuted[pattern[list[i]]] = arg
-        }
-        action.build(permuted)
-      }
-
-    }
-    if (optional) {
-      this.optional << list
-    }
+    queries.expect(pattern, action, optional)
     return this
   }
 
@@ -83,44 +63,10 @@ class Descriptor {
     pings.contains(message)
   }
 
-  private boolean isSatisfied(Construction c, ParsingContext ctx) {
-    return obligatoryPatterns().every { !ctx.strongUsages(c, expectations[it].name).isEmpty() }
-  }
-
-  private List<List> obligatoryPatterns() {
-    return (expectations.keySet() as List) - optional
-  }
-
-  private boolean reparse(Construction c, ParsingContext ctx) {
-    for (descr in expectations.values()) {
-      ctx.relaxUsages(descr.name)
-    }
-
-    while (true) {
-      Map<Set<Construction>, List<Pair<List, List<Construction>>>> cons2Patterns = [:]
-      expectations.each { pattern, descr ->
-        if (!ctx.strongUsages(c, descr.name)) {
-          ctx.match(pattern).each { args ->
-            args.each { cons2Patterns.get(ctx.cloud.competitors[it], []) << new Pair(pattern, args) }
-          }
-        }
-      }
-      def single = cons2Patterns.find { k, v -> v.size() == 1 }
-      if (single) {
-        def pair = single.value[0]
-        ctx.addConstruction(expectations[pair.fst].build(pair.snd))
-      } else {
-        break
-      }
-    }
-
-    return processExpectationsEagerly(c, ctx)
-  }
-
   boolean activate(Construction c, ParsingContext ctx) {
-    if (ctx.cloud.finished.contains(c) && !isSatisfied(c, ctx)) {
+    if (ctx.cloud.finished.contains(c) && !queries.isSatisfied(c, ctx)) {
       ctx.cloud.finished.remove c
-      return reparse(c, ctx)
+      return queries.reparse(c, ctx)
     }
 
     if (_replaces >= 0) {
@@ -137,41 +83,16 @@ class Descriptor {
 
     demotedArgs.each { ctx.demote(c.args[it]) }
 
-    boolean happy = processExpectationsEagerly(c, ctx)
+    boolean happy = queries.processExpectationsEagerly(c, ctx)
     if (happy && famous && ctx.usages(c, []).findAll { it.consumed(c) }.isEmpty()) {
       return false
     }
     return happy
   }
 
-  private boolean processExpectationsEagerly(Construction c, ParsingContext ctx) {
-    def happy = true
-    expectations.each {pattern, Descriptor builder ->
-      if (!ctx.strongUsages(c, builder.name)) {
-        def argLists = ctx.match(pattern)
-        if (argLists) {
-          argLists.each {args ->
-            ctx.addConstruction builder.build(args)
-          }
-        } else {
-          if (pattern.indexOf("_") == 0 || ctx.cloud.activeBefore(ctx.cloud.ranges[c].fromInt).size() > 0) {
-            if (!optional.contains(pattern)) {
-              if (c.tracked) {
-                c
-              }
-              happy = false
-            }
-          }
-        }
-      }
-    }
-    return happy
-  }
-
   boolean shouldActivate() {
-    return famous || !expectations.isEmpty()
+    return famous || !queries.isEmpty()
   }
-
 
   boolean isTracked() { tracked }
 
@@ -221,19 +142,7 @@ class Descriptor {
   }
 
   Descriptor evokes(Descriptor parent, int argNumber, boolean optional = false) {
-    parent._patterns.each {
-      def copy = it.clone()
-      if (copy instanceof List) {
-        copy[argNumber] = "_"
-        expect(copy, parent, optional)
-      } else {
-        assert copy instanceof Map
-        def key = copy.find { k, v -> v == argNumber }.key
-        copy.remove key
-        copy["_"] = argNumber
-        expect(copy, parent, optional)
-      }
-    }
+    queries.evokes(parent, argNumber, optional)
     this
   }
 

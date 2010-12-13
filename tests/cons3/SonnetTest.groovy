39190c66c4b840e7ccbc3e7f1bdd36333d989b5b
peter <peter.gromov@jetbrains.com>
2010-12-13 11:38:54 +0300

    english-based mentalese

--- tests/cons3/SonnetTest.groovy
+++ tests/cons3/SonnetTest.groovy
@@ -10,30 +10,30 @@ class SonnetTest extends TestCase {
   public void testParse1() throws Exception {
     String input = "Удивительный случай случился со мной: я вдруг забыл, что идет раньше - 7 или 8"
     String expected = """
-A.property:=УДИВИТЕЛЬНЫЙ
-A.type:=СЛУЧАЙ
-B.type:=СЛУЧИТЬСЯ
+A.property:=AMAZING
+A.type:=THING
+B.type:=HAPPEN
 this.time==PAST
 B.theme:=A
-C.type==Я
+C.type==ME
 B.experiencer:=C
 this.elaboration:=#2
 --
-A.type==Я
-B.manner:=ВДРУГ
+A.type==ME
+B.manner:=SUDDENLY
 B.experiencer:=A
-B.type:=ЗАБЫТЬ
+B.type:=FORGET
 this.time==PAST
 B.theme:=#3
 --
 this.questioned:=A
-B.type==ИДТИ_В_ПОСЛЕДОВАТЕЛЬНОСТИ
+B.type==COME_SCALARLY
 this.time==PRESENT
 B.theme==A
-B.order==РАНЬШЕ
+B.order==EARLIER
 A.variant==7
 A.variant==8
 """
-    assertEquals expected.trim(), new Parser().parse(input)
+    assertEquals expected.trim(), new Parser().parse(input).presentable()
   }
 }

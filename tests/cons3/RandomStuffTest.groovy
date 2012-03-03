7cfaee91b74f07b639fc32fcb705628ab8b6155c
peter <peter@jetbrains.com>
2012-03-03 18:59:35 +0400

    vasya also had a melon

--- tests/cons3/RandomStuffTest.groovy
+++ tests/cons3/RandomStuffTest.groovy
@@ -16,4 +16,8 @@ class RandomStuffTest extends TestCase {
     doTranslateTest 'У меня был арбуз.',
             'I had a water melon.'
   }
+  public void testVasyaHadMelon() throws Exception {
+    doTranslateTest 'У Васи был арбуз.',
+            'Vasya had a water melon.'
+  }
 }

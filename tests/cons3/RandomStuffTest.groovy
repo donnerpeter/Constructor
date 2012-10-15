bc6cbefa88531576b569f63fa097cf5164e2a8a1
peter <peter@jetbrains.com>
2012-10-15 22:21:49 +0200

    no throws Exception

--- tests/cons3/RandomStuffTest.groovy
+++ tests/cons3/RandomStuffTest.groovy
@@ -8,15 +8,15 @@ import static cons3.SonnetTest.doTranslateTest
  * @author peter
  */
 class RandomStuffTest extends TestCase {
-  public void testIHaveMelon() throws Exception {
+  public void testIHaveMelon() {
     doTranslateTest 'У меня есть арбуз.',
             'I have a water melon.'
   }
-  public void testIHadMelon() throws Exception {
+  public void testIHadMelon() {
     doTranslateTest 'У меня был арбуз.',
             'I had a water melon.'
   }
-  public void testVasyaHadMelon() throws Exception {
+  public void testVasyaHadMelon() {
     doTranslateTest 'У Васи был арбуз.',
             'Vasya had a water melon.'
   }

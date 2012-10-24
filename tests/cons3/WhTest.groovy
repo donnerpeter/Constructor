9390d2e430f5d6e93472de4fbf443e3321c0e708
peter <peter@jetbrains.com>
2012-10-24 09:55:55 +0200

    WhTest

---
+++ tests/cons3/WhTest.groovy
@@ -0,0 +1,36 @@
+package cons3
+
+import junit.framework.TestCase
+
+import static cons3.SonnetTest.doTranslateTest
+
+/**
+ * @author peter
+ */
+class WhTest extends TestCase {
+  public void "test what comes first 7 or 8"() {
+    doTranslateTest "Что идёт раньше - 7 или 8?",
+            "What comes first - 7 or 8?"
+  }
+  public void "test what do they think on this matter"() {
+    doTranslateTest "Что они думают по этому поводу?",
+            "What do they think on this matter?"
+  }
+  public void "_test what do they think on this matter"() {
+    doTranslateTest "Кому она сломала челюсть?",
+            "What do they think on this matter?"
+  }
+  public void "__test what do they think on this matter"() {
+    doTranslateTest "Что она ему сломала?",
+            "What do they think on this matter?"
+  }
+  public void "___test what do they think on this matter"() {
+    doTranslateTest "Кого он спросил?",
+            "What do they think on this matter?"
+  }
+  public void "____test what do they think on this matter"() {
+    doTranslateTest "О чём он спросил соседей?",
+            "What do they think on this matter?"
+  }
+
+}

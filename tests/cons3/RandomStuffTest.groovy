d888fe1700266ceb38fb014780cd3b5b5cf90948
peter <peter@jetbrains.com>
2012-11-04 17:53:58 +0100

    todo I'm on the corner tests

--- tests/cons3/RandomStuffTest.groovy
+++ tests/cons3/RandomStuffTest.groovy
@@ -62,4 +62,12 @@ class RandomStuffTest extends TestCase {
     doTranslateTest 'Он велел помочь ей',
             "To drink tea is to live long"
   }
+  public void "_test I'm on the corner"() {
+    doTranslateTest 'Я на углу',
+            "I'm on the corner"
+  }
+  public void "_test I'm already on the corner of Znamenskaya and Basseynaya streets"() {
+    doTranslateTest 'Я уже на углу Бассейной и Знаменской улицы',
+            "I'm already on the corner of Znamenskaya and Basseynaya streets"
+  }
 }

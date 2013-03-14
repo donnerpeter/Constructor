27177b4d69f370df64f5456c0eb44a6065eeae84
peter <peter@jetbrains.com>
2013-03-14 16:55:10 +0100

    copy some gpp interfaces

--- src3/cons3/Interfaces.groovy
+++ src3/cons3/Interfaces.groovy
@@ -6,3 +6,15 @@ package cons3
 interface Predicate1<T> {
   boolean call(T arg)
 }
+interface Function2<T1,T2,V> {
+  V call(T1 t1, T2 t2)
+}
+interface Function3<T1,T2,T3,V> {
+  V call(T1 t1, T2 t2, T3 t3)
+}
+interface Function4<T1,T2,T3,T4,V> {
+  V call(T1 t1, T2 t2, T3 t3, T4 t4)
+}
+interface Function1<T1,V> {
+  V call(T1 t1)
+}

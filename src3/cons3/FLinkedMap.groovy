c6c6a811becb3d46651df97bc8ad8866e0abdf91
peter <peter@jetbrains.com>
2013-03-15 11:47:34 +0100

    FLinkedMap based on HashPMap

--- src3/cons3/FLinkedMap.groovy
+++ src3/cons3/FLinkedMap.groovy
@@ -1,31 +1,31 @@
 package cons3
 
 import org.pcollections.Empty
+import org.pcollections.PMap
 import org.pcollections.PSequence
 
 /**
  * @author peter
  */
+
 final class FLinkedMap<K,V> implements Map<K,V> {
-  private static final FLinkedMap empty = new FLinkedMap(Empty.stack())
-  private final PSequence<MyEntry<K, V>> list
+  private static final FLinkedMap empty = new FLinkedMap(Empty.stack(), Empty.map())
+  private final PSequence<K> list
+  private final PMap<K, V> map
 
   static <K, V> FLinkedMap<K, V> getEmptyMap() {
     empty
   }
 
-  private FLinkedMap(PSequence<MyEntry<K, V>> list) {
+  private FLinkedMap(PSequence<K> list, PMap<K, V> map) {
     this.list = list
+    this.map = map
+    assert map != null
   }
 
   FLinkedMap<K, V> putValue(K key, V value) {
-    def newEntry = new MyEntry(key, value)
-    def newList = list
-    def existing = list.find { it.key == key }
-    if (existing) {
-      newList -= existing;
-    }
-    return new FLinkedMap<K,V>(newList + newEntry)
+    def newList = map.containsKey(key) ? list : list.plus(key)
+    return new FLinkedMap<K,V>(newList, map.plus(key, value))
   }
 
   FLinkedMap<K, V> minus(K key) {
@@ -33,24 +33,17 @@ final class FLinkedMap<K,V> implements Map<K,V> {
   }
 
   FLinkedMap<K, V> removeKey(K key) {
-    MyEntry<K,V> existing = list.find { it.key == key }
-    if (existing) {
-      return new FLinkedMap<K,V>(list - existing)
-    }
-    return this
+    return new FLinkedMap<K, V>(list - key, map - key)
   }
 
-  FLinkedMap<K, V> removeAll(Iterable<K> key) {
-    Reference<FLinkedMap<K, V>> result = [this]
-    key.each { result.set(result.get().removeKey(it)) }
-    return result.get()
+  FLinkedMap<K, V> removeAll(Collection<K> key) {
+    return new FLinkedMap<K, V>(list.minusAll(key), map.minusAll(key))
   }
-
 
   FLinkedMap<K, V> plus(Map<K, V> map) {
-    Reference<FLinkedMap<K, V>> ref = [this]
-    map.each { k, v -> ref.set(ref.get().putValue(k, v)) }
-    return ref.get()
+    FLinkedMap<K, V> result = this
+    map.each { k, v -> result = result.putValue(k, v) }
+    return result
   }
 
   static <K, V> FLinkedMap<K, V> fromMapReverse(Map<K, V> map) {
@@ -58,8 +51,7 @@ final class FLinkedMap<K,V> implements Map<K,V> {
   }
 
   V get(Object key) {
-    def entry = list.find { it.key == key }
-    return entry?.value
+    return map.get(key)
   }
 
   V getAt(K key) {
@@ -70,29 +62,20 @@ final class FLinkedMap<K,V> implements Map<K,V> {
     return get(key)
   }
 
-  @Override
-  String toString() {
-    return "{ " + list.collect { it.toString() }.join(", ") + "}"
-  }
-
-  List<K> keyList() {
-    list.collect { it.key }
-  }
-
   int size() {
-    list.size
+    map.size()
   }
 
   boolean isEmpty() {
-    list.empty
+    map.isEmpty()
   }
 
   boolean containsKey(Object o) {
-    list.find { it.key == o } != null
+    map.containsKey(o)
   }
 
   boolean containsValue(Object o) {
-    list.find { it.value == o } != null
+    map.containsValue(o)
   }
 
   V remove(Object o) {
@@ -108,15 +91,20 @@ final class FLinkedMap<K,V> implements Map<K,V> {
   }
 
   Set<K> keySet() {
-    Collections.unmodifiableSet(new LinkedHashSet<K>(list.collect {it.key})) //todo 'as LinkedHashSet' slow: asType
+    Collections.unmodifiableSet(new LinkedHashSet<K>(list))
   }
 
   Collection<V> values() {
-    Collections.unmodifiableList(list.collect {it.value} as List)
+    PMap<K, V> _map = map
+    Collections.unmodifiableList(list.collect { _map.get(it) } as List) //todo bug
   }
 
   Set<Map.Entry<K, V>> entrySet() {
-    Collections.unmodifiableSet(new LinkedHashSet<Map.Entry<K, V>>(list))
+    Map<K,V> ordered = new LinkedHashMap<K,V>()
+    for (key in keySet()) {
+      ordered[key] = map.get(key)
+    }
+    Collections.unmodifiableSet(ordered.entrySet())
   }
 
   V put(K k, V v) {
@@ -124,28 +112,7 @@ final class FLinkedMap<K,V> implements Map<K,V> {
   }
 
   FLinkedMap<K, V> reverse() {
-    return new FLinkedMap(Util.reverse(list))
-  }
-
-  private static class MyEntry<K, V> implements Map.Entry<K, V> {
-    final K key
-    final V value
-
-    MyEntry(K key, V value) {
-      this.key = key
-      this.value = value
-    }
-
-    V setValue(V v) {
-      throw new UnsupportedOperationException("setValue is not implemented")
-    }
-
-    @Override
-    String toString() {
-      return "$key -> $value"
-    }
-
-
+    return new FLinkedMap(Util.reverse(list), map)
   }
 
 }

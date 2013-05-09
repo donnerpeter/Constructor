a3e9db6e8d2fc92db58ab02ec3d316b5f6e4c989
peter <peter@jetbrains.com>
2013-05-09 23:29:31 +0200

    shorter Assignment

--- src3/cons3/Assignment.kt
+++ src3/cons3/Assignment.kt
@@ -7,8 +7,10 @@ data class Assignment<T>(
         val frame: T,
         val property: String,
         val value: Any,
-        val generation: Int,
-        val section: Int) {
+        generation: Int,
+        section: Int) {
+  val generation = generation
+  val section = section
 
   fun toString(): String = "${frameId(frame)}.$property=${stringValue()}"
 
@@ -18,9 +20,4 @@ data class Assignment<T>(
     return if (value is Variable) value.getId()!! else (value as Frame).getVar()!!.getId()!!
   }
 
-  fun equals(o: Any): Boolean =
-    o is Assignment<*> && o.frame == frame && o.property == property && o.value == value
-
-  fun hashCode() : Int =
-          (frame.hashCode() * 31 + property.hashCode()) * 31 + value.hashCode()
 }

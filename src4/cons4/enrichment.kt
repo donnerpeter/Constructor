36a2972c77c42dbf69deba179c929eb6313a8358
peter <peter@jetbrains.com>
2013-05-12 10:54:46 +0200

    specify arg1 relation in the lexical entry

--- src4/cons4/enrichment.kt
+++ src4/cons4/enrichment.kt
@@ -8,10 +8,6 @@ import cons4.Variable
 fun enrichMites(cxts: List<Construction>) = cxts.flatMap { enrich(it) }
 
 fun enrich(cxt: Construction): List<Construction> {
-  if (cxt is nom && cxt.head != null && cxt.noun != null) {
-    return listOf(sem(cxt.head, "arg1", cxt.noun))
-  }
-
   return when (cxt) {
     is word -> handleWord(cxt.word!!)
     else -> ArrayList()
@@ -20,9 +16,10 @@ fun enrich(cxt: Construction): List<Construction> {
 
 fun handleWord(w: String): List<Construction> {
   val v = Variable()
+  val v2 = Variable()
   return when (w) {
     "случай" -> listOf(nom(noun = v), sem(v, "type", "THING"))
-    "случился" -> listOf(nom(head = v), sem(v, "type", "HAPPEN"))
+    "случился" -> listOf(nom(head = v, noun = v2.lightVar), sem(v, "type", "HAPPEN"), sem(v, "arg1", v2))
     "удивительный" -> listOf(nom(noun = v.lightVar), sem(v, "property", "AMAZING"))
     else -> ArrayList()
   }

836c6ae594e7b221ab7e972d9cc7cbe177a818cc
peter <peter@jetbrains.com>
2013-05-19 23:20:34 +0200

    finiteVerb&l

--- src4/cons4/lexicon.kt
+++ src4/cons4/lexicon.kt
@@ -10,24 +10,23 @@ fun handleWord(w: String): List<Mite> {
   val v = Vars()
   val v0 = v[0]
   return when (w) {
-    "вдруг" -> listOf(verb("verb" to v0.lightVar)) + sem(v0, "manner" to "SUDDENLY")
-    "забыл" -> listOf(verb("verb" to v0), nom("head" to v0, "noun" to v[nom].lightVar)) +
-          sem(v0, "type" to "FORGET", "time" to "PAST", "arg1" to v[nom])
-    "идет" -> listOf(verb("verb" to v0), nom("head" to v0, "noun" to v[nom].lightVar), comeScalarly("verb" to v0)) +
-          sem(v0, "time" to "PRESENT", "arg1" to v[nom])
+    "вдруг" -> l(verb("verb" to v0.lightVar)) + sem(v0, "manner" to "SUDDENLY")
+    "забыл" -> finiteVerb(v, "PAST") + sem(v0, "type" to "FORGET")
+    "идет" -> finiteVerb(v, "PRESENT") + l(comeScalarly("verb" to v0))
     "мной" -> pronoun(v, instr, "ME")
-    "раньше" -> listOf(comeScalarly("verb" to v0.lightVar, "order" to "EARLIER"))
+    "раньше" -> l(comeScalarly("verb" to v0.lightVar, "order" to "EARLIER"))
     "случай" -> noun(v, nom, "THING")
-    "случился" -> listOf(verb("verb" to v0), nom("head" to v0, "noun" to v[nom].lightVar), sInstr("head" to v0, "noun" to v[sInstr].lightVar)) +
-      sem(v0, "type" to "HAPPEN", "time" to "PAST", "arg1" to v[nom], "experiencer" to v[sInstr])
-    "со" -> listOf(sInstr("noun" to v0), instr("noun" to v0.lightVar, "head" to Any()))
-    "удивительный" -> listOf(nom("noun" to v0.lightVar), sem(v0, "property", "AMAZING"))
-    "что" -> listOf(nom("noun" to v0), sem.t(v0, "wh"))
+    "случился" -> finiteVerb(v, "PAST") + l(sInstr("head" to v0, "noun" to v[sInstr].lightVar)) + sem(v0, "type" to "HAPPEN", "experiencer" to v[sInstr])
+    "со" -> l(sInstr("noun" to v0), instr("noun" to v0.lightVar, "head" to Any()))
+    "удивительный" -> l(nom("noun" to v0.lightVar), sem(v0, "property", "AMAZING"))
+    "что" -> pronoun(v, nom, "wh")
     "я" -> pronoun(v, nom, "ME")
-    ":" -> listOf(verb("verb" to v0.lightVar, "last" to true), verb("verb" to v[1].lightVar, "first" to true), sem(v0, "elaboration", v[1]))
-    else -> ArrayList()
+    ":" -> l(verb("verb" to v0.lightVar, "last" to true), verb("verb" to v[1].lightVar, "first" to true), sem(v0, "elaboration", v[1]))
+    else -> l()
   }
 }
 
-fun noun(v: Vars, case: Construction, typ: String) = listOf(case("noun" to v[0]), sem.t(v[0], typ))
+fun l(vararg mites: Mite) = listOf(*mites)
+fun noun(v: Vars, case: Construction, typ: String) = l(case("noun" to v[0]), sem.t(v[0], typ))
 fun pronoun(v: Vars, case: Construction, typ: String) = noun(v, case, typ)
+fun finiteVerb(v: Vars, time: String) = l(verb("verb" to v[0]), nom("head" to v[0], "noun" to v[nom].lightVar)) + sem(v[0], "time" to time, "arg1" to v[nom])

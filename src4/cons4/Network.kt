b76306959c12c44a4455c9dd0d18b5ff08cc7d5c
peter <peter@jetbrains.com>
2013-06-16 17:01:30 +0200

    faster contradiction

--- src4/cons4/Network.kt
+++ src4/cons4/Network.kt
@@ -4,7 +4,9 @@ import java.util.LinkedHashMap
 import java.util.LinkedHashSet
 import java.util.HashMap
 
-data class Network(val parents: Map<Mite, List<Mite>> = LinkedHashMap(), val children: Map<Mite, List<Mite>> = LinkedHashMap()) {
+data class Network(val parents: Map<Mite, List<Mite>> = LinkedHashMap(),
+                   val children: Map<Mite, List<Mite>> = LinkedHashMap(),
+                   private val contrCache: HashMap<Pair<Mite, Mite>, Boolean> = HashMap()) {
   fun addRelation(parent: Mite, child: Mite): Network {
     val newParents = LinkedHashMap(parents)
     val newChildren = LinkedHashMap(children)
@@ -13,8 +15,8 @@ data class Network(val parents: Map<Mite, List<Mite>> = LinkedHashMap(), val chi
     return copy(parents = newParents, children = newChildren)
   }
 
-  fun getParents(mite: Mite): List<Mite> = parents[mite] ?: listOf()
-  fun getChildren(mite: Mite): List<Mite> = children[mite] ?: listOf()
+  fun getParents(mite: Mite): List<Mite> = parents[mite].orEmpty()
+  fun getChildren(mite: Mite): List<Mite> = children[mite].orEmpty()
 
   fun addMite(mite: Mite): Network {
     if (mite.src1 == null) return this
@@ -33,26 +35,23 @@ data class Network(val parents: Map<Mite, List<Mite>> = LinkedHashMap(), val chi
 
   fun findContradictors(mite: Mite, among: Collection<Mite>) = among.filter { it == mite || contradict(mite, it) }
 
-  private val contrCache = HashMap<Pair<Mite, Mite>, Boolean>()
   fun contradict(mite1: Mite, mite2: Mite): Boolean {
     return contrCache.getOrPut(mite1 to mite2) { _contradict(mite1, mite2) }
   }
   private fun _contradict(mite1: Mite, mite2: Mite): Boolean {
     if (mite1.contradicts(mite2)) return true
 
-    for (group in getDirectPreconditionGroups(mite1)) {
-      if (group.all { contradict(it, mite2) }) return true
+    for (atom in mite1.primaries) {
+      val parents = getParents(atom)
+      if (parents.notEmpty() && parents.all { contradict(it, mite2) }) return true
     }
-
-    for (group in getDirectPreconditionGroups(mite2)) {
-      if (group.all { contradict(mite1, it) }) return true
+    for (atom in mite2.primaries) {
+      val parents = getParents(atom)
+      if (parents.notEmpty() && parents.all { contradict(mite1, it) }) return true
     }
 
     return false
   }
-
-  private fun getDirectPreconditionGroups(mite: Mite): List<List<Mite>> = mite.primaries.map { getParents(it) }.filter { it.notEmpty() }
-
 
 }
 

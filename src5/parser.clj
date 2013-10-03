6557184e534985c10571f91ce5e43250a831479c
peter <peter@jetbrains.com>
2013-10-03 14:49:39 +0200

    some more lexical entries

--- src5/parser.clj
+++ src5/parser.clj
@@ -17,6 +17,9 @@
     (cons (mite :sem :frame var :attr (first pairs) :value (nth pairs 1)) (apply sem var (drop 2 pairs)))))
 (defn adj [case var rel value] (cons (mite case :child var) (sem var rel value)))
 (defn noun [case var type] (cons (mite case :child var) (sem var "type" type)))
+(defn pronoun [case var type] (cons (mite case :child var) (sem var "type" type)))
+(defn preposition [v prepCxt case] [(mite prepCxt :child (v 0)) (mite case :child (v 0 :light) :head (v 1))])
+(defn arg [v case rel] (cons (mite case :head (v 0) :child (v case :light)) (sem (v 0) rel (v case))))
 (defn finiteVerb [v time type]
   (concat
     (list (mite :phrase :head (v 0) :kind "verb") (mite :nom :head (v :nom)))
@@ -28,10 +31,12 @@
             ([key light] (. (. _vars get key) getLv)))
         var (v 0)]
     (case (clojure.string/lower-case word)
-    "случай" (noun :nom var "THING")
-    "случился" (finiteVerb v "PAST" "HAPPEN")
-    "удивительный" (adj :nom (v 0 :light) "property" "AMAZING")
-    '())))
+      "мной" (pronoun :instr var "ME")
+      "случай" (noun :nom var "THING")
+      "случился" (concat (finiteVerb v "PAST" "HAPPEN") (arg v :sInstr "experiencer"))
+      "со" (preposition v :sInstr :instr)
+      "удивительный" (adj :nom (v 0 :light) "property" "AMAZING")
+      '())))
 
 (defn enrich [mite]
   (case (:cxt mite)

37db04bc19fda222afe07daca89d62c80187a33a
peter <peter@jetbrains.com>
2013-11-01 18:49:05 +0100

    trees rewritten

--- src5/parsingState.clj
+++ src5/parsingState.clj
@@ -11,14 +11,38 @@
 (defrecord ParsingState [trees log enrich])
 (defn empty-parsing-state [enrich] (->ParsingState () "" enrich))
 
-(defrecord Tree [root mites active contradictor-map left-child right-child])
-(defn empty-tree [root] (->Tree root [] #{} {} nil nil))
+(defrecord Node [root mites contradictor-map left right]
+  Object (toString [_] (str root)))
+(defn empty-node [root] (->Node root [] {} [] []))
+
+(defrecord Tree [root nodes active]
+  Object (toString [_] (str root " " nodes)))
+(defn get-node [tree mite]
+  (let [result ((.nodes tree) mite)]
+    (assert result
+            (str "no " mite " in " tree))
+    result)
+  )
+(defn root-node [tree] (get-node tree (.root tree)))
+
+(defn visible-node-mites [^Tree tree ^Node node]
+  (let [left-mites (apply concat (map #(.mites (first %)) (.left node)))
+        right-mites (apply concat (map #(.mites (first %)) (.right node)))]
+    (concat (.mites node) left-mites right-mites))
+  )
+(defn all-tree-mites [tree]
+  (letfn [(inner [node]
+                 (let [left-mites (apply concat (map #(inner (get-node tree (second %))) (.left node)))
+                       right-mites (apply concat (map #(inner (get-node tree (second %))) (.right node)))]
+                   (concat left-mites (visible-node-mites tree node) right-mites))
+                 )]
+    (inner (root-node tree))))
 
 (defn append-log [state newLog]
   (assoc state :log
     (str (.log state) newLog "\n")))
 (defn print-log [state] (println (str "Log:" (:log state))))
-(defn all-mites [state] (flatten (reverse (map #(:mites %) (:trees state)))))
+(defn all-mites [state] (flatten (reverse (map #(all-tree-mites %) (.trees state)))))
 (defn get-chart [state]
   (let [kotlin-cxt (fn [cxt] (cond
                                (= cxt :sem) (. sem instance$)
@@ -31,30 +55,27 @@
         kotlin-mites (map #(new cons4.Mite (kotlin-cxt (.cxt %)) (to-linked-map (.args %)) nil nil nil) active)]
     (new Chart kotlin-mites)))
 
-(defn all-tree-mites [tree] (.mites tree))
-(defn tree-core [tree]
-  (if (.right-child tree)
-    (tree-core (if (is-left-headed? (.root tree)) (.left-child tree) (.right-child tree)))
-    tree))
-(defn visible-simple-trees [tree]
-  (if-let [right (.right-child tree)]
-    (concat (visible-simple-trees right)
-            (let [res [tree]
-                  res (if (is-left-headed? (.root tree)) (concat res [(tree-core tree)]) res)
-                  res (if (and (.right-child (.left-child tree)) (not (is-left-headed? (.root (.left-child tree))))) (concat res [(.left-child tree)]) res)]
-              res))
-    [tree]))
-(defn visible-tree-mites [tree] (mapcat #(.mites %) (visible-simple-trees tree)))
+(defn visible-nodes [tree from-right]
+  (letfn [(inner [node]
+                 (let [res [node]
+                       [next-edge & _] (if from-right (.right node) (.left node))
+                       res (if next-edge
+                             (concat (inner (get-node tree (second next-edge))) res)
+                             res)]
+                   res))]
+    (inner (root-node tree))))
+
+(defn visible-tree-mites [tree] (mapcat #(visible-node-mites tree %) (visible-nodes tree true)))
 (defn visible-mites [state] (visible-tree-mites (first (.trees state))))
 
 (defn present-mite [mite active] (str (if active "*" "") (if (is-happy? mite) "" "!") mite))
 (defn present-tree [tree]
   (let [active (.active tree)
-        present-level (fn [tree]
+        present-level (fn [node]
                         (str "  "
-                             (clojure.string/join " " (map #(present-mite % (in? active %)) (.mites tree)))))
+                             (clojure.string/join " " (map #(present-mite % (in? active %)) (visible-node-mites tree node)))))
         ]
-    (clojure.string/join "\n" (map present-level (visible-simple-trees tree)))
+    (clojure.string/join "\n" (map present-level (visible-nodes tree true)))
   ))
 
 (defn presentable [state]
@@ -65,49 +86,49 @@
     (str (clojure.string/join "\n" (map present-tree (.trees state))) additional-str)))
 
 (defn find-contradictors [mite coll] (filter #(and (not= mite %) (mites-contradict mite %)) coll))
-(defn contradictors [tree mite] (get (:contradictor-map tree) mite))
-(defn happy-contradictors [tree mite] (filter #(is-happy? %) (contradictors tree mite)))
+(defn contradictors [node mite] (get (:contradictor-map node) mite))
+(defn happy-contradictors [node mite] (filter #(is-happy? %) (contradictors node mite)))
 
 (defrecord ActiveChange [chosen remaining uncovered])
 
-(defn is-uncovered? [mite tree chosen-map]
+(defn is-uncovered? [mite node chosen-map]
   (and
-    (every? #(= false (get chosen-map %)) (happy-contradictors tree mite))
+    (every? #(= false (get chosen-map %)) (happy-contradictors node mite))
     (or
       (= false (get chosen-map mite))
       (not (is-happy? mite)))
     ))
 
-(defn update-uncovered [expelled-coll tree ac]
-  (let [suspicious (clojure.set/union (set (mapcat #(contradictors tree %) expelled-coll)) expelled-coll)
-        fresh-uncovered (filter #(is-uncovered? % tree (:chosen ac)) suspicious)]
+(defn update-uncovered [expelled-coll node ac]
+  (let [suspicious (clojure.set/union (set (mapcat #(contradictors node %) expelled-coll)) expelled-coll)
+        fresh-uncovered (filter #(is-uncovered? % node (:chosen ac)) suspicious)]
     (assoc ac :uncovered (clojure.set/union (:uncovered ac) fresh-uncovered))))
 
 (defn is-complete-change? [ac] (empty? (:remaining ac)))
-(defn fork-change [tree ac]
+(defn fork-change [node ac]
   (let [mite (first (:remaining ac))
         rest-remaining (rest (:remaining ac))
         taken (let [to-expel (filter #(mites-contradict mite %) rest-remaining)
                     expelled-map (reduce #(assoc %1 %2 false) (:chosen ac) to-expel)
                     ]
-                (update-uncovered to-expel tree (assoc ac :chosen (assoc expelled-map mite true) :remaining (filter #(not (in? to-expel %)) rest-remaining))))
-        omitted (update-uncovered [mite] tree (assoc ac :chosen (assoc (:chosen ac) mite false) :remaining rest-remaining))]
+                (update-uncovered to-expel node (assoc ac :chosen (assoc expelled-map mite true) :remaining (filter #(not (in? to-expel %)) rest-remaining))))
+        omitted (update-uncovered [mite] node (assoc ac :chosen (assoc (:chosen ac) mite false) :remaining rest-remaining))]
     [taken omitted]))
 
-(defn apply-change [ac tree]
-  (let [all (.mites tree)
+(defn apply-change [ac node]
+  (let [all (.mites node)
         uncovered (:uncovered ac)
         new-active (filter
                      #(or (in? uncovered %) (= true (get (:chosen ac) %))) all)]
     new-active))
 
-(defn build-contradictor-cache [tree]
-  (let [all (all-tree-mites tree)]
+(defn build-contradictor-cache [node]
+  (let [all (.mites node)]
     (zipmap all (map #(find-contradictors % all) all))))
 
 (defn suggest-active [tree]
   (let [visible (visible-tree-mites tree)
-        all (:mites tree)
+        all (all-tree-mites tree)
         invisible (set (filter #(not (in? visible %)) all))
         all-unhappy (filter #(not (is-happy? %)) all)
         all-happy (filter #(is-happy? %) all)
@@ -117,7 +138,8 @@
                           [invisible-uncovered (- (count uncovered) invisible-uncovered)]))
         initial-change (->ActiveChange {} all-happy #{})
         ]
-    (loop [queue (priority-map initial-change (change-weight initial-change))]
+    (set all)
+    #_(loop [queue (priority-map initial-change (change-weight initial-change))]
       (let [[next-ac & weight] (peek queue)
             queue (pop queue)]
         (if (is-complete-change? next-ac)
@@ -128,38 +150,56 @@
       ))
   )
 
-(defn init-tree [root enrich]
+(defn init-node [root enrich]
   (loop [all-mites [] to-enrich [root]]
     (if (empty? to-enrich)
-      (assoc (empty-tree root) :mites (vec all-mites))
+      (assoc (empty-node root) :mites (vec all-mites))
       (recur (concat all-mites to-enrich)
              (flatten (map enrich to-enrich))))))
 
-(defn new-leaf-tree [^mites.Mite root enrich]
-  (let [tree (init-tree root enrich)
-        tree (assoc tree :contradictor-map (build-contradictor-cache tree))
+(defn new-leaf-tree [root-mite enrich]
+  (let [node (init-node root-mite enrich)
+        node (assoc node :contradictor-map (build-contradictor-cache node))
+        tree (->Tree root-mite {root-mite node} #{})
         active (suggest-active tree)]
     (assoc tree :active active)))
 
 (defn do-merge-trees [state ^Tree left-tree ^Tree right-tree]
-  (let [all-unified (flatten (for [right (visible-tree-mites right-tree)
-                                   left (visible-tree-mites left-tree)]
-                               (if-let [unified (unify left right)] [unified] ())
-                               ))]
-    (for [merged all-unified]
-      (let [left-headed (is-left-headed? merged)
-            merged-tree (init-tree merged (.enrich state))
-            merged-tree (assoc merged-tree :left-child left-tree :right-child right-tree)
-            active (suggest-active merged-tree)
-            merged-tree (assoc merged-tree :active active)
-            ]
-        merged-tree))))
+  (let [left-visible (visible-nodes left-tree true)
+        right-visible (visible-nodes right-tree false)
+        left-top (root-node left-tree)
+        right-top (root-node right-tree)
+        merge-node-mites (fn [^Node left-node ^Node right-node]
+                           (flatten
+                             (for [right (visible-node-mites right-tree right-node)
+                                          left (visible-node-mites left-tree left-node)]
+                                      (if-let [unified (unify left right)] [unified] ())
+                                      )))
+        create-merged-tree (fn [merged-mite child-node head-node direction]
+                             (let [merged-node (init-node merged-mite (.enrich state))
+                                   head-mite (.root head-node)
+                                   head-node (assoc head-node direction (cons [merged-node (.root child-node)] (direction head-node)))
+                                   nodes-map (merge (.nodes left-tree) (.nodes right-tree))
+                                   nodes-map (assoc nodes-map merged-mite merged-node head-mite head-node)
+                                   merged-tree (assoc (if (= :right direction) left-tree right-tree) :nodes nodes-map)
+                                   active (suggest-active merged-tree)
+                                   ]
+                               (assoc merged-tree :active active))
+                             )
+        right-headed-merges (for [right-node right-visible
+                                 merged (merge-node-mites left-top right-node) :when (not (is-left-headed? merged))]
+                             (create-merged-tree merged left-top right-node :left))
+        left-headed-merges (for [left-node left-visible
+                                  merged (merge-node-mites left-node right-top) :when (is-left-headed? merged)]
+                              (create-merged-tree merged right-top left-node :right))
+        ]
+    (concat left-headed-merges right-headed-merges)))
 
 (defn merge-trees [state]
-  (let [[right & prev-trees] (:trees state)]
-    (if (empty? prev-trees)
+  (let [[right & [left & prev-trees]] (.trees state)]
+    (if (nil? left)
       state
-      (let [all-trees (do-merge-trees state (first prev-trees) right)
+      (let [all-trees (do-merge-trees state left right)
             all-states (map #(assoc state :trees (cons % (rest prev-trees))) all-trees)]
         (if (empty? all-states) state (merge-trees (first all-states)))))))
 
--- test2/parsingStateTest.clj
+++ test2/parsingStateTest.clj
@@ -10,8 +10,7 @@
            ([key light] (. (. _vars get key) getLv)))))
 
 (defn add-mites [state mites]
-  (let [tree (new-leaf-tree (first mites) (fn [mite] (if (= mite (first mites)) (rest mites) [])))
-        tree (assoc tree :mites mites)]
+  (let [tree (new-leaf-tree (first mites) (fn [mite] (if (= mite (first mites)) (rest mites) [])))]
     (add-tree state tree)))
 
 (deftest headless-merge
@@ -33,7 +32,7 @@
         mite2 (mite :nom :child (v 1))
         state (add-word state mite1)
         state (add-word state mite2)]
-    (is (= [mite2 (unify mite1 mite2) mite1] (visible-mites state)))
+    (is (= [mite2 mite1 (unify mite1 mite2)] (visible-mites state)))
     ))
 (deftest right&left
   (let [mite1 (mite :nom :child (v 0))
@@ -43,7 +42,7 @@
         state (add-mites state mites2)
         state (add-mites state [mite3])
         ]
-    (is (= (concat [mite3 (unify (mites2 1) mite3)] mites2 [(unify mite1 (mites2 0))]) (visible-mites state)))
+    (is (= (concat [mite3] mites2 [(unify mite1 (mites2 0)) (unify (mites2 1) mite3)]) (visible-mites state)))
     ))
 (deftest left&left-same-head
   (let [mites1 [(mite :nom :head (v 0)) (mite :acc :head (v 0))]
@@ -53,5 +52,15 @@
         state (add-mites state [mite2])
         state (add-mites state [mite3])
         ]
-    (is (= (concat [mite3 (unify (mites1 1) mite3)] mites1) (visible-mites state)))
+    (is (= (concat [mite3] mites1 [(unify (mites1 1) mite3) (unify (mites1 0) mite2)]) (visible-mites state)))
+    ))
+(deftest left&left-chaining
+  (let [mite1 (mite :nom :head (v 0))
+        mites2 [(mite :nom :child (v 1)) (mite :acc :head (v 1))]
+        mite3 (mite :acc :child (v 2))
+        state (add-mites state [mite1])
+        state (add-mites state mites2)
+        state (add-mites state [mite3])
+        ]
+    (is (= (concat [mite3] mites2 [(unify (mites2 1) mite3) mite1 (unify mite1 (mites2 0))]) (visible-mites state)))
     ))

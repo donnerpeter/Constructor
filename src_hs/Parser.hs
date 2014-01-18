6ac990dc2bf0108c050652e86eccde5b817f917f
peter <peter@jetbrains.com>
2014-01-18 13:39:30 +0100

    more tests

--- src_hs/Parser.hs
+++ src_hs/Parser.hs
@@ -36,17 +36,55 @@ sonnetTests = [
           "I went to my neighbors and asked them about their opinion on this matter"
   ]
 
-sonnetVariationTests=[
+variationTests1=[
 --  translateTest "Удивительный случай случился со мной: я вдруг забыл, что идет раньше - 7 или 8" 
---          "An amazing thing happened to me today, I suddenly forgot what comes first - 7 or 8",
---  translateTest "Я вдруг забыл, что - 7 или 8 - идет раньше" 
---         "I suddenly forgot what comes first - 7 or 8",
---  translateTest "Я вдруг забыл, что, 7 или 8, идет раньше" 
---         "I suddenly forgot what comes first - 7 or 8",
+--          "An amazing thing happened to me today, I suddenly forgot what comes first - 7 or 8"
+--  ,
+--  translateTest "Я вдруг забыл, что - 7 или 8 - идет раньше" "I suddenly forgot what comes first - 7 or 8"
+--  ,
+--  translateTest "Я вдруг забыл, что, 7 или 8, идет раньше" "I suddenly forgot what comes first - 7 or 8"
+--  ,
 --  translateTest "Я вдруг забыл, кого я видел - кассиршу или соседей" 
 --         "I suddenly forgot who I saw, the cashier or my neighbors"
+--  ,
+--  translateTest "Я забыл, что идёт раньше - 7, 8 или 9." "I forgot what comes first - 7, 8 or 9."
+--  ,
+--  translateTest "Я забыл, что 7 идёт раньше 8." "I forgot, that a 7 comes before an 8."
+--  ,
+--  translateTest "Я забыл, что 7 идёт раньше 8 и что 4 идёт после 3."
+--                "I forgot, that a 7 comes before an 8 and that a 4 comes after a 3."
+--  ,
+--  translateTest "Я забыл, что я это забыл" "I forgot, that I forgot that"
+--  ,
+--  translateTest "Я забыл, что я забыл это" "I forgot, that I forgot that"
+--  ,
+--  translateTest "Я забыл, что я забыл" "I forgot what I forgot"
+--  ,
+--  translateTest "Я забыл, что я забыл, что 7 идет раньше 8" "I forgot, that I forgot, that a 7 comes before an 8"
+--  ,
+--  translateTest "Я забыл, что я забыл, что идет после 8" "I forgot, that I forgot what comes after an 8"
+--  ,
+--  translateTest "Я забыл, что я делал."
+--                    "I forgot what I did."
+--  ,
+--  translateTest "Я забыл, что я делал, что идет после 8 и что идет раньше 7."
+--                    "I forgot what I did, what comes after an 8, and what comes before a 7."
+--  ,
+--  translateTest "Я забыл, что я забыл, что идет после 8 и что идет раньше 7"
+--                    "I forgot, that I forgot what comes after an 8 and what comes before a 7"
+--  ,
+--  translateTest "Я забыл, что идёт после 7 и что идёт раньше 8." "I forgot what comes after a 7 and what comes before an 8."
+--  ,
+--  translateTest "Я забыл, что 8 идёт после 7 и что идёт раньше 5."
+--            "I forgot, that an 8 comes after a 7 and what comes before a 5."
+--  ,
+--  translateTest "Я забыл, что идёт после 7 и что 4 идёт раньше 5."
+--            "I forgot what comes after a 7 and that a 4 comes before a 5."
   ]
-
-tests = TestList (sonnetTests++sonnetVariationTests)
+variationTests2=[
+--  translateTest "Он отправился к соседям" 
+--                "He went to his neighbors"
+  ]
+tests = TestList (sonnetTests++variationTests1++variationTests2)
 
 allTests = runTestTT tests

572276e9f816dd4d845a42ebd797a9053c0b118d
peter <peter@jetbrains.com>
2014-03-04 22:52:21 +0100

    prettier time output

--- src_hs/runghc.sh
+++ src_hs/runghc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ghc -O2 -odir ../out -hidir ../out -o ../out/Main Main && time ../out/Main 2>&1 | ./pretty.sh
+ghc -O2 -odir ../out -hidir ../out -o ../out/Main Main && time -f "elapsed %E" ../out/Main 2>&1 | ./pretty.sh
 
 # profiling
 # ghc -O2 -prof -fprof-auto -rtsopts -odir ../out -hidir ../out -o ../out/Main Main && ../out/Main +RTS -p 2>&1 | ./pretty.sh

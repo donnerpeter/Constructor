c5e769c2e2a15c50fbcc413b03ac54a2199f097a
peter <peter@jetbrains.com>
2014-09-09 08:51:42 +0400

    longer profiling

--- src_hs/Profile.hs
+++ src_hs/Profile.hs
@@ -4,4 +4,7 @@ import Constructor.Lexicon
 import Constructor.Parser
 import Constructor.EnglishGenerator
 
-main = putStrLn $ translate "Кассир сказал, что семь, по его мнению, идет после восьми в том случае, когда восемь идет после семи"
+toTranslate = ["Кассир сказал, что семь, по его мнению, идет после восьми в том случае, когда восемь идет после семи",
+               "Кассирша сказала, подвигав носом, что, по ее мнению, семь идет после восьми в том случае, когда восемь идет после семи"]
+
+main = putStrLn $ show $ map translate $ concat $ replicate 10 toTranslate

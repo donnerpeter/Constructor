6c2d803ce016486dbfd59a2e2ed5391a69ee6335
peter <peter@jetbrains.com>
2014-11-18 21:17:45 +0100

    faster profiling

--- src_hs/Profile.hs
+++ src_hs/Profile.hs
@@ -7,4 +7,4 @@ import Constructor.EnglishGenerator
 toTranslate = ["Кассир сказал, что семь, по его мнению, идет после восьми в том случае, когда восемь идет после семи",
                "Кассирша сказала, подвигав носом, что, по ее мнению, семь идет после восьми в том случае, когда восемь идет после семи"]
 
-main = putStrLn $ show $ map translate $ concat $ replicate 10 toTranslate
+main = putStrLn $ show $ map translate toTranslate

6c2f4c9e7cb5b945a3a766b8308d0398cc850c2c
peter <peter@jetbrains.com>
2015-01-02 11:14:21 +0100

    relax expected times

--- src_hs/Constructor/Tests/Testing.hs
+++ src_hs/Constructor/Tests/Testing.hs
@@ -2,7 +2,7 @@ module Constructor.Tests.Testing where
 
 data TranslateTest = TranslateTest { src:: String, target:: String, timeLimit:: Int }
 
-translateTest src target = textTranslateTest 100 src target
+translateTest src target = textTranslateTest 200 src target
 
 textTranslateTest time src target = TranslateTest src target time
 

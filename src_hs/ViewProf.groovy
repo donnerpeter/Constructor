31625d5abf76f379cc42a6b5fc1b9cea5f299d2d
peter <peter@jetbrains.com>
2015-02-21 16:30:27 +0100

    ViewProf: support pa format

--- src_hs/ViewProf.groovy
+++ src_hs/ViewProf.groovy
@@ -4,26 +4,35 @@ import javax.swing.*
 import javax.swing.tree.DefaultMutableTreeNode
 import javax.swing.tree.DefaultTreeModel
 import java.awt.*
+import java.text.DecimalFormat
 import java.util.List
 
-@Canonical class CallInfo { String place; String module; int entries; double totalTime;
+@Canonical class CallInfo { String place; String module; int entries; int totalTime; int totalBytes;
   List<CallInfo> callees = []
   String getId() { module + '.' + place }
-  String toString() { return "$place ($module)  time $totalTime%, count $entries" }
+  String toString() {
+    def result = "$place ($module)  time ${totalTime}ms"
+    if (entries > 0) result += ",     count " + entries
+    int kbytes = totalBytes/1024;
+    if (kbytes > 0) result += ",     kbytes " + new DecimalFormat("#,###").format(kbytes)
+    return result
+  }
 }
 
 class MergedCallInfo extends CallInfo {
   MergedCallInfo(List<CallInfo> components) {
-    super(components[0].place, components[0].module, components.collect{ it.entries}.sum() as int, components.collect{ it.totalTime}.sum() as double)
+    super(components[0].place, components[0].module, components.collect{ it.entries}.sum() as int, components.collect{ it.totalTime}.sum(), components.collect{ it.totalBytes}.sum())
   }
 }
 
 @SuppressWarnings(["GrReassignedInClosureLocalVar", "GroovyMissingReturnStatement"])
 CallInfo readProfile() {
+  int hadCostCentre = 0
   boolean started = false
   List<CallInfo> nodeStack = [new CallInfo("root", "root", 0, 0)]
-  new File("profiled.prof").eachLine { line ->
-    if (line.startsWith("MAIN")) started = true
+  new File(args[0]).eachLine { line ->
+    if (line.startsWith("COST CENTRE")) hadCostCentre++
+    if (hadCostCentre == 2 && line.startsWith("MAIN")) started = true
     if (!started) return
 
     int prefixLength = 0
@@ -31,8 +40,15 @@ CallInfo readProfile() {
     if (prefixLength == 0) return
 
     def areas = line.split(" +")
-    def newNode = new CallInfo(place:areas[1], module:areas[2], entries:(areas[4] as double) as int, totalTime:areas[7] as double)
 
+    def ownTime = areas[9] as int
+    def ownBytes = areas[10] as int
+    def newNode = new CallInfo(place:areas[1], module:areas[2], entries:areas[4] as int, totalTime:ownTime, totalBytes:ownBytes)
+
+    for (i in 0..<prefixLength) {
+      nodeStack[i].totalTime += ownTime
+      nodeStack[i].totalBytes += ownBytes
+    }
     def parent = nodeStack[prefixLength - 1]
     parent.callees.add(newNode)
     if (nodeStack.size() <= prefixLength) nodeStack.add(null)
@@ -85,5 +101,5 @@ tabs.addTab("Plain tree", new JScrollPane(tree))
 tabs.addTab("Method list", new JScrollPane(jList))
 frame.contentPane.add(tabs)
 
-frame.size = new Dimension(800, 500)
+frame.size = new Dimension(1000, 500)
 frame.visible = true
--- src_hs/runghc.sh
+++ src_hs/runghc.sh
@@ -5,7 +5,7 @@
 if [ $PROFILE ]; then
   echo profiling
   cabal configure --enable-executable-profiling
-  cabal build profiled && time -f "elapsed %E" dist/build/profiled/profiled +RTS -p 2>&1 | ./pretty.sh
+  cabal build profiled && time -f "elapsed %E" dist/build/profiled/profiled +RTS -pa 2>&1 | ./pretty.sh
 else
   cabal configure --disable-executable-profiling
   cabal build constructor && time -f "elapsed %E" dist/build/constructor/constructor 2>&1 | ./pretty.sh

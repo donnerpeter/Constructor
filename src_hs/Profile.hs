0b8e9ceea110fe5b859559e6fa45fc6033645e62
peter <peter@jetbrains.com>
2015-02-26 13:47:46 +0100

    profile more with less overhead

--- src_hs/Profile.hs
+++ src_hs/Profile.hs
@@ -3,8 +3,10 @@ import Constructor.ParsingState
 import Constructor.Lexicon
 import Constructor.Parser
 import Constructor.EnglishGenerator
+import Constructor.Tests.Sonnet
 
-toTranslate = ["Кассир сказал, что семь, по его мнению, идет после восьми в том случае, когда восемь идет после семи",
+toTranslate = [sonnetText,
+               "Кассир сказал, что семь, по его мнению, идет после восьми в том случае, когда восемь идет после семи",
                "Кассирша сказала, подвигав носом, что, по ее мнению, семь идет после восьми в том случае, когда восемь идет после семи"]
 
 main = putStrLn $ show $ map translate toTranslate

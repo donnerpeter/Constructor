a3f7537fd810f4e3f6d94062a13c659aafe78205
peter <peter@jetbrains.com>
2015-02-28 18:11:55 +0100

    faster variant sorting

--- src_hs/Constructor/ParsingState.hs
+++ src_hs/Constructor/ParsingState.hs
@@ -63,7 +63,8 @@ chooseBestLastVariants finalHistory allVariants = {-trace (length result) -}resu
   dup = findDuplicate competitors
   nodups = if isJust dup then error ("duplicate " ++ show dup) else competitors
   isStableTree tree = all (isStable . cxt) $ mites tree
-  sortedVariants = sortBy (compare `on` (length . mergedRoots)) $ sortBy (compare `on` mergedUnhappyCount) $ sortBy (compare `on` mergedIssueCount) nodups
+  weight tree = (length (mergedRoots tree), mergedUnhappyCount tree, mergedIssueCount tree)
+  sortedVariants = sortBy (compare `on` weight) nodups
   result = head sortedVariants : metricAscending (metric $ head sortedVariants) (tail sortedVariants)
 
   metricAscending _ [] = []

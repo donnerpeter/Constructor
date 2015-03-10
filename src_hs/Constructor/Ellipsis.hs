3a0d554e0c0c982487ac6d276e06d0d451b06e7a
peter <peter@jetbrains.com>
2015-03-10 09:47:32 +0100

    ellipsis refactoring

--- src_hs/Constructor/Ellipsis.hs
+++ src_hs/Constructor/Ellipsis.hs
@@ -6,6 +6,7 @@ import Constructor.Variable
 import Constructor.InteractionEnv
 import Constructor.Constructions
 import Data.Maybe
+import Data.List (find)
 import qualified Constructor.SemanticProperties as P
 
 enumerateActiveMites tree includeSelf = let
@@ -18,7 +19,12 @@ findClause tree = [(m, t) | (m@(cxt -> Clause _), t) <- enumerateActiveMites tre
 
 suggestEllipsis env ellipsisVar e1 e2 = case findClause =<< context env of
   [] -> []
-  (oldClause, tree):_ -> xor $ processEllipsis oldClause ellipsisVar e1 e2 tree
+  (oldClause, tree):_ -> let
+    allMites = allTreeMites tree
+    mappings = catMaybes [processEllipsis oldClause ellipsisVar [mapping1, mapping2] tree |
+       mapping1 <- findOriginals allMites e1,
+       mapping2 <- findOriginals allMites e2]
+    in xor mappings
 
 data AnchorMapping = AnchorMapping { templateMite:: Mite, templateVar:: Variable, anchorVar:: Variable } deriving (Show)
 
@@ -31,42 +37,34 @@ checkOriginal anchor candidate = case (cxt candidate, anchor) of
 
 findOriginals mites anchor = catMaybes $ map (checkOriginal anchor) mites
 
-processEllipsis :: Mite -> Variable -> Construction -> Construction -> Tree -> [[Mite]]
-processEllipsis oldClause ellipsisVar@(Variable varIndex _) e1 e2 prevTree = let
+processEllipsis :: Mite -> Variable -> [AnchorMapping] -> Tree -> Maybe [Mite]
+processEllipsis oldClause ellipsisVar@(Variable varIndex _) mappings prevTree = let
   Clause oldCP = cxt oldClause
-  allMites = allTreeMites prevTree
-  mappings = catMaybes [mapConstructions mapping1 mapping2 | mapping1 <- findOriginals allMites e1, mapping2 <- findOriginals allMites e2]
-  mapConstructions :: AnchorMapping -> AnchorMapping -> Maybe [Mite]
-  mapConstructions m1 m2 = let
-    o1 = cxt (templateMite m1)
-    o2 = cxt (templateMite m2)
-    mapVariable _v =
-      if _v == templateVar m1 then anchorVar m1
-      else if _v == templateVar m2 then anchorVar m2
-      else if _v == oldCP then ellipsisVar
-      else Variable varIndex ("_" ++ show _v)
-    mapMite m = case cxt m of
-      Unify _v1 _v2 -> [mite $ Unify (mapVariable _v1) (mapVariable _v2)]
-      Sem _v1 (StrValue attr s) ->
-        if attr == P.RusNumber || attr == P.RusGender || attr == P.RusPerson then []
-        else [semS (mapVariable _v1) attr s] ++ (if attr == P.Type then [semS (mapVariable _v1) P.Elided "true"] else [])
-      Sem _v1 (VarValue attr _v2)  -> [mite $ Sem (mapVariable _v1) (VarValue attr $ mapVariable _v2)]
-      _ -> []
-    walkTree :: Tree -> ([Mite], [Construction])
-    walkTree tree = let
-      activeHeadMites = Constructor.Tree.activeHeadMites tree
-      headCxts = map cxt activeHeadMites
-      ownMapped = filter (not . contradict oldClause) activeHeadMites >>= mapMite
-      folder (allMapped, allCovered) tree = let (mapped, covered) = walkTree tree in (allMapped++mapped, allCovered++covered)
-      (leftMapped, leftCovered) = foldl folder ([], []) $ subTrees LeftSide tree
-      (rightMapped, rightCovered) = foldl folder ([], []) $ subTrees RightSide tree
-      in
-      if elem o1 headCxts then ([], [o1])
-      else if elem o2 headCxts then ([], [o2])
-      else if containsClause activeHeadMites then ([], [])
-      else (leftMapped++ownMapped++rightMapped, leftCovered++rightCovered)
-    (mapped, covered) = walkTree prevTree
-    containsClause mites = not $ null [m | m@(cxt -> Clause cp) <- mites, oldCP /=cp]
-    in
-    if covered == [o1,o2] then Just mapped else Nothing
-  in mappings
+  templateCxts = map (cxt . templateMite) mappings
+  mapVariable _v = case find (\m -> _v == templateVar m) mappings of
+    Just m -> anchorVar m
+    _ -> if _v == oldCP then ellipsisVar else Variable varIndex ("_" ++ show _v)
+  mapMite m = case cxt m of
+    Unify _v1 _v2 -> [mite $ Unify (mapVariable _v1) (mapVariable _v2)]
+    Sem _v1 (StrValue attr s) ->
+      if attr == P.RusNumber || attr == P.RusGender || attr == P.RusPerson then []
+      else [semS (mapVariable _v1) attr s] ++ (if attr == P.Type then [semS (mapVariable _v1) P.Elided "true"] else [])
+    Sem _v1 (VarValue attr _v2)  -> [mite $ Sem (mapVariable _v1) (VarValue attr $ mapVariable _v2)]
+    _ -> []
+  walkTree :: Tree -> ([Mite], [Construction])
+  walkTree tree = let
+    activeHeadMites = Constructor.Tree.activeHeadMites tree
+    headCxts = map cxt activeHeadMites
+    ownMapped = filter (not . contradict oldClause) activeHeadMites >>= mapMite
+    folder (allMapped, allCovered) tree = let (mapped, covered) = walkTree tree in (allMapped++mapped, allCovered++covered)
+    (leftMapped, leftCovered) = foldl folder ([], []) $ subTrees LeftSide tree
+    (rightMapped, rightCovered) = foldl folder ([], []) $ subTrees RightSide tree
+    in case find (`elem` headCxts) templateCxts of
+      Just original -> ([], [original])
+      _ ->
+        if containsClause activeHeadMites then ([], [])
+        else (leftMapped++ownMapped++rightMapped, leftCovered++rightCovered)
+  (mapped, covered) = walkTree prevTree
+  containsClause mites = not $ null [m | m@(cxt -> Clause cp) <- mites, oldCP /=cp]
+  in
+  if covered == templateCxts then Just mapped else Nothing
